fc57c1bfc5a219960df68036ecb4e2d91029b91b
Aaron Benyamini <aaronben@umich.edu>
2023-04-06 11:09:31 -0700

    commit push

--- HW7.py
+++ HW7.py
@@ -53,7 +53,28 @@ def make_positions_table(data, cur, conn):
 #     created for you -- see make_positions_table above for details.
 
 def make_players_table(data, cur, conn):
-    pass
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Players (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            position_id INTEGER,
+            birthyear INTEGER,
+            nationality TEXT,
+            FOREIGN KEY (position_id) REFERENCES Positions(id)
+        )
+    ''')
+
+    for player in data['squad']:
+        id = player['id']
+        name = player['name']
+        position = player['position']
+        dob = player['dateOfBirth'][:4]
+        nationality = player['nationality']
+        cur.execute("SELECT id FROM Positions WHERE position=?", (position,))
+        id_position = cur.fetchone()[0]
+        cur.execute("INSERT OR IGNORE INTO Players (id, name, position_id, birthyear, nationality) VALUES (?, ?, ?, ?, ?)",(id, name, id_position, dob, nationality))
+        
+    conn.commit()
 
 ## [TASK 2]: 10 points
 # Finish the function nationality_search
